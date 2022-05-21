0abf63cd3564f7780538d1e40d54871cca47ef78
seungyeop-lee <xbeast7@gmail.com>
2022-05-21 20:25:34 +0900

    session 유지 확인 endpoint 추가

--- spring_dev/src/main/java/com/github/seungyeop_lee/project_dev_on_docker/spring_dev/controller/HelloController.java
+++ spring_dev/src/main/java/com/github/seungyeop_lee/project_dev_on_docker/spring_dev/controller/HelloController.java
@@ -1,14 +1,34 @@
 package com.github.seungyeop_lee.project_dev_on_docker.spring_dev.controller;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.io.Serializable;
 
 @RestController
 @RequestMapping("/hello")
+@RequiredArgsConstructor
 public class HelloController {
+    private final NameStore nameStore;
+
     @GetMapping("")
-    public String hello() {
-        return "hello!";
+    public String hello(@RequestParam(required = false) String name) {
+        if (name != null) {
+            nameStore.setName(name);
+        }
+        return "hello " + nameStore.getName() + "!";
+    }
+
+    @Component
+    @SessionScope
+    @Data
+    public static class NameStore implements Serializable {
+        private String name;
     }
 }
